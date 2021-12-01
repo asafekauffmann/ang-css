65332999925ec7fa189fc58ba863a54f30406c73
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-12-01 15:41:21 -0300

    up

--- src/app/abertura/encap/encap.component.ts
+++ src/app/abertura/encap/encap.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-encap',
-  templateUrl: './encap.component.html',
-  styleUrls: ['./encap.component.sass']
+   selector: 'app-encap',
+   templateUrl: './encap.component.html',
+   styleUrls: ['./encap.component.sass']
 })
 export class EncapComponent implements OnInit {
 
-  constructor() { }
+   storedTheme: string | null  = localStorage.getItem('theme-color');
 
-  ngOnInit(): void {
-  }
+   constructor() { }
+
+   ngOnInit(): void {
+   }
+
+   setTheme() {
+      if(this.storedTheme === 'theme-dark') {
+         localStorage.setItem('theme-color', 'theme-light');
+         this.storedTheme = localStorage.getItem('theme-color');
+      } else {
+         localStorage.setItem('theme-color', 'theme-dark');
+         this.storedTheme = localStorage.getItem('theme-color');
+
+      }
+   }
 
 }
